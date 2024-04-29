33434f67ba5d9e811df15a69e5062da553c1688d
AOMG <agustin@cloudeho.com>
2024-04-29 13:33:11 -0600

    connect to db

--- configs/setup.go
+++ configs/setup.go
@@ -3,7 +3,6 @@ package configs
 import (
 	"context"
 	"log"
-	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -11,27 +10,12 @@ import (
 
 func ConnectDB() *mongo.Client {
 
-	//TODO no mames la variable de ambiente cn la URI no puede ser global.
-
-	client, err := mongo.NewClient(options.Client().ApplyURI(""))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	connString := getConnString()
+	clientOpts := options.Client().ApplyURI(
+		connString)
+	client, err := mongo.Connect(context.TODO(), clientOpts)
 	if err != nil {
 
-		log.Fatal(err)
-	}
-
-	//ping the database
-	err = client.Ping(ctx, nil)
-	if err != nil {
 		log.Fatal(err)
 	}
 	return client
--- controllers/quotesC.go
+++ controllers/quotesC.go
@@ -10,6 +10,8 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -44,8 +46,78 @@ func NewQuote() gin.HandlerFunc {
 	}
 }
 
-// ** updateQuote
-//** getQuote
-//** searhcQuote
-//** deleteQuote
-//**searchQuoteClient
+func GetQuotes() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		cid := c.GetHeader("cid")
+		var quotes []models.Quote
+		defer cancel()
+
+		results, err := quotesCollection.Find(ctx, bson.M{"cid": cid})
+
+		defer results.Close(ctx)
+		if err = results.All(context.TODO(), &quotes); err != nil {
+			c.JSON(http.StatusInternalServerError, responses.PMGResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+			return
+		}
+
+		c.JSON(http.StatusOK, responses.PMGResponse{Status: http.StatusOK, Message: "success", Data: map[string]interface{}{"data": quotes}})
+
+	}
+}
+
+func UpdateQuote() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		cid := c.GetHeader("cid")
+		var quote models.Quote
+		defer cancel()
+
+		//validate the request body
+		if err := c.BindJSON(&quote); err != nil {
+			c.JSON(http.StatusBadRequest, responses.PMGResponse{Status: http.StatusBadRequest, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+			return
+		}
+
+		//use the validator library to validate required fields
+		if validationErr := validateQuotes.Struct(&quote); validationErr != nil {
+			c.JSON(http.StatusBadRequest, responses.PMGResponse{Status: http.StatusBadRequest, Message: "error", Data: map[string]interface{}{"data": validationErr.Error()}})
+			return
+		}
+
+		quoteUpdated, err := quotesCollection.UpdateOne(ctx, bson.M{"cid": cid}, bson.M{"$set": quote})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, responses.PMGResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+			return
+		}
+		c.JSON(http.StatusOK, responses.PMGResponse{Status: http.StatusOK, Message: "success", Data: map[string]interface{}{"data": quoteUpdated}})
+
+	}
+}
+
+func DeleteAQuote() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		cid := c.Param("cid")
+		defer cancel()
+
+		objId, _ := primitive.ObjectIDFromHex(cid)
+
+		quoteDeleted, err := quotesCollection.DeleteOne(ctx, bson.M{"id": objId})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, responses.PMGResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+			return
+		}
+
+		if quoteDeleted.DeletedCount < 1 {
+			c.JSON(http.StatusNotFound,
+				responses.PMGResponse{Status: http.StatusNotFound, Message: "error", Data: map[string]interface{}{"data": 0}},
+			)
+			return
+		}
+
+		c.JSON(http.StatusOK,
+			responses.PMGResponse{Status: http.StatusOK, Message: "success", Data: map[string]interface{}{"data": quoteDeleted.DeletedCount}},
+		)
+	}
+}
